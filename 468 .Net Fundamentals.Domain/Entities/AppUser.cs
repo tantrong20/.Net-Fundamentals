039ccbca31388f975cf432b12facdddc4f0c2fce
TanTrong <tronglt2001@gmail.com>
2022-01-16 16:03:09 +0700

    Create Model AppUser

---
+++ 468 .Net Fundamentals.Domain/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _468_.Net_Fundamentals.Domain.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public string ImagePath { get; set; }
+    }
+}
